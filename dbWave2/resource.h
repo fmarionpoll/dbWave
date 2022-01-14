2cd5db101d0fdba0476c41fd766607b838af3829
Frederic Marion-Poll <marion_poll@hotmail.com>
2022-01-14 17:29:58 +0100

    change resources.h

--- dbWave2/resource.h
+++ dbWave2/resource.h
@@ -936,7 +936,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_3D_CONTROLS                     1
+#define _APS_3D_CONTROLS                1
 #define _APS_NEXT_RESOURCE_VALUE        577
 #define _APS_NEXT_COMMAND_VALUE         33021
 #define _APS_NEXT_CONTROL_VALUE         1516
