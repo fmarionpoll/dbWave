75fb056466b261e65f0463c590704ddae87a5e98
Frederic Marion-Poll <marion_poll@hotmail.com>
2022-09-18 23:48:54 +0200

    cosmetic change

--- dbWave2/SpikeClassDescriptor.h
+++ dbWave2/SpikeClassDescriptor.h
@@ -16,8 +16,10 @@ public:
 
 	int get_class_id() const {return class_id; }
 	void set_class_id(const int id) { class_id = id; }
+
 	int get_n_items() const {return n_items; }
 	void set_n_items(const int n) { n_items = n; }
+
 	int increment_n_items() { n_items++; return n_items; }
 	int decrement_n_items() { if (n_items > 0)  n_items--; return n_items; }
 };
